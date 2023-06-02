757d382ef71a673e5f931c3efb12cdaee9133a45
Henrique <hlopessantos52@gmail.com>
2023-06-02 07:21:11 -0300

    comment web.php and add new line to new TaskController

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
 
+Route::get('/tasks', [\App\Http\Controllers\TasksController::class, 'index']);
