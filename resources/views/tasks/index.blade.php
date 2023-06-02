d46ce58f10619faf987968b53eee867035df82f8
Henrique <hlopessantos52@gmail.com>
2023-06-02 08:28:26 -0300

    add form to index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,9 +5,20 @@
 
     <h1>Lista de Tarefas</h1>
 
+
+    <form action="{{ route('tasks.store') }}" method="post">
+        @csrf
+        <div>
+            <label title="title">Titulo da Tarefa</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <button type="submit">Adicionar Tarefa</button>
+    </form>
+
     <ul>
         @foreach($tasks as $task)
             <li> {{$task}} </li>
         @endforeach
     </ul>
+
 </x-layout>
